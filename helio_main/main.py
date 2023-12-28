b0cc000f664f3a00ab5e78284a10e5355b6ddaa4
Gabriel Mango <gabrielhmango@gmail.com>
2023-12-28 20:42:28 -0300

    feat: implament Helio prompt apresentation

--- helio_main/main.py
+++ helio_main/main.py
@@ -1,6 +1,19 @@
+import os
+from user import User
+
 def main():
-    ...
+
+    # Start the apresentation of Helio
+    print('HELIO: Hello! My name is Helio.')
+    print(
+        'HELIO: I am a tool designed to help professionals '
+        'who work with relational databases.'
+        '\nHELIO: Lets start!'
+    )
+
+
 
 
 if __name__ == '__main__':
+    os.system('cls')
     main()
